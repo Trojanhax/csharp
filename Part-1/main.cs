90f4bd067569bf84222d475df77c9bf3ab1afd0d
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-14 20:35:53 +0530

    Create main.cs

---
+++ Part-1/main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello,world");
+            Console.Write("hello");
+            Console.WriteLine("hello,world");
+            Console.ReadLine();
+            Console.Beep();
+        }
+    }
+}
