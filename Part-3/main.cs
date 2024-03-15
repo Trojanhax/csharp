66d62c4aa7dd066f328f62161638e77fdd80607a
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-15 16:40:53 +0530

    Create main.cs

---
+++ Part-3/main.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //  constants in c#:
+
+            const double pi = 3.1415;
+            // pi = 456;
+
+
+            Console.WriteLine("value NOT changed " + pi);
+
+
+
+            Console.ReadKey(); 
+
+        }
+    }
+}
