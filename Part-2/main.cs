7b9945084fb4f068d1a6b09d50ac4b1b4ab67ca8
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-15 16:33:35 +0530

    Create main.cs

---
+++ Part-2/main.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //variable in c# 
+            int X; // declaration
+            X = 123; // intst
+
+            int a = 34; // integer
+
+            double age = 34.5; // double
+
+            char b = 'g'; // character
+
+            String c = " gray"; // string
+
+            bool isGratet =  true; // boolean
+
+            float d = 10; /// floAT
+
+
+            Console.WriteLine( "your age is " + X); 
+            Console.WriteLine(a);
+            Console.WriteLine("yoyr hight is "+age+ " cm");
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(isGratet);
+            Console.WriteLine(d);
+
+            // to stop console``
+            Console.ReadKey(); 
+
+        }
+    }
+}
