ef72dc2753ec8a25d3024fa6341a9423ca267df4
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-16 22:14:22 +0530

    Create main.cs

---
+++ Part-15/main.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop in c#
+
+            // for (int i = 0; i <= 100; i++)
+            //for (int i = 0; i <= 100; i +=3)
+            //    {
+            //    Console.WriteLine(i);
+            //}
+
+
+            for (int i = 100; i >= 0; i--)
+            //for (int i = 100; i >= 0; i -= 3)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
+  
