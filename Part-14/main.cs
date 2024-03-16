15dce6241cde0654eb3300dc528ce77296496d8e
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-16 22:04:02 +0530

    Create main.cs

---
+++ Part-14/main.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //
+            // While loop in c#
+
+            Console.Write("Enter your name : ");
+            string name = Console.ReadLine();
+
+            //while (name == "")
+            //{
+             //   Console.Write("Enter your name : ");
+             //   name = Console.ReadLine();
+
+            //}
+
+            while (1 == 1)
+            {
+                Console.Write("Enter your name : ");
+                
+
+            }
+
+            Console.WriteLine("Hello " + name);
+
+
+
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
+  
