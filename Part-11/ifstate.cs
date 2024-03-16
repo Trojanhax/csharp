9285a0b1da343a48eeb230bcc4c9be1ea67a738f
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-16 16:22:27 +0530

    Create ifstate.cs

---
+++ Part-11/ifstate.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //
+            // if else and else if 
+            //
+
+            Console.WriteLine("Enter your age :  ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("you are now sign up !");
+
+            }
+
+
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
+  
