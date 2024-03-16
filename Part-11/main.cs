e3bf3c14e17334b9566c83b010f1e268777b3ff5
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-16 16:29:47 +0530

    Create main.cs

---
+++ Part-11/main.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //
+            // if else and else if 
+            //
+
+            Console.WriteLine("Enter your age :  ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("you are now sign up !");
+
+            }
+            else if (age < 100)
+            {
+                Console.WriteLine("you are too old");
+            }
+            else if (age >0)
+            {
+                Console.WriteLine("you have not born yet ");
+
+            }
+            else
+            {
+                Console.WriteLine("you must be 18 to sign up");
+            }
+
+
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
+  
