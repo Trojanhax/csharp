3cc0567faaabaa6e925d443f38ba8a8272f6201d
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-20 18:14:21 +0530

    Create Massage.cs

---
+++ Part-31/Massage.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Massage
+    {
+        public static void hello()
+        {
+            Console.WriteLine("hello");
+        }
+        static void Wating()
+        {
+            Console.WriteLine("wating for something");
+        }
+        static void Bye()
+        {
+            Console.WriteLine("Bye Bye");
+        }
+    }
+}
