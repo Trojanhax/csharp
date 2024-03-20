0e6ce5d036b97e140dde39f9f624b76ca38d2126
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-20 18:13:39 +0530

    Create Program.cs

---
+++ Part-31/Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////
+            /// class(oop) in c#
+            /// 
+
+            Math.Round(3.14);
+            Math.Abs(-3.14);
+            Math.Pow(2, 3);
+            Massage.hello();
+
+
+
+            
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
