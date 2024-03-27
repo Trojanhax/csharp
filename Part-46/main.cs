01696f30c3630dbb50d9e26aac54bedc5e6d0a64
Trojan hax <124779020+Trojanhax@users.noreply.github.com>
2024-03-27 15:35:58 +0530

    Update main.cs

--- Part-46/main.cs
+++ Part-46/main.cs
@@ -1,1 +1,51 @@
+using System;
+using System.Collections.Generic;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////
+            /// list of objects in c#
+            /// 
+            List<Player> players = new List<Player>();
+
+            Player player1 = new Player("chad");
+            Player player2 = new Player("abc");
+            Player player3 = new Player("cxyz");
+
+            players.Add(player1);
+            /*players.Add(player1);
+            players.Add(player1);
+            players.Add(player1);*/
+            players.Add(player2);
+            players.Add(player3);
+
+            foreach(Player player in players)
+            {
+                Console.WriteLine(player);
+            }
+
+
+
+            Console.ReadKey();
+        }
+       
+    }
+    class Player
+    {
+        public string username;
+        public Player(string username)
+        {
+            this.username = username;
+        }
+        public override string ToString()
+        {
+            return $"{username}";
+        }
+    }
+    
+}
+ 
